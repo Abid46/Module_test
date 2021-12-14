8a3469f0ec77d5bd669d3f263d932ccf13745662
Sayed <mdsayedgv2@gmail.com>
2021-12-14 17:25:19 +0600

    first commit

---
+++ index.js
@@ -0,0 +1,8 @@
+// import {text} from '/indexM.js'
+// console.log(text);
+
+// let texts = "Module not work";
+// console.log(texts);
+
+const data = require("./STUDY WITH ANIS.json");
+console.log("data");
